c6c23d79468703d485cd8cd5aa81e2723c59850c
erdoganakpolat <erdoganakpolat98@gmail.com>
2026-02-03 18:20:38 +0300

    new servlet has been added.

--- ServletDemo/src/main/java/servlets/HelloServlet.java
+++ ServletDemo/src/main/java/servlets/HelloServlet.java
@@ -1,4 +1,4 @@
-package erdogan;
+package servlets;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -6,19 +6,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
- * Servlet implementation class FirstServlet
+ * Servlet implementation class HelloServlet
  */
-@WebServlet("/FirstServlet")
-public class FirstServlet extends HttpServlet {
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public FirstServlet() {
+    public HelloServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,10 +26,8 @@ public class FirstServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter pw = response.getWriter();
-		pw.print("<h1> Hello");
-		
-	//	response.getWriter().append("Served at: ").append(request.getContextPath());
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
